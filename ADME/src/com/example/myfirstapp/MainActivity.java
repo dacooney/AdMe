c2ced5e49657329427ac8881bcfb8910b74e0d50
James <walshj19@tcd.ie>
2013-06-15 18:36:29 +0100

    Initial commit

---
+++ ADME/src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	/*
+	 * Logs the user in and starts the app.
+	 */
+	public void login(View view){
+		
+	}
+
+}
