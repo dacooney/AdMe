2bfcd191f54ba3a8c84d7f86f033390903fa293b
dacooney <daniel.cooney92@gmail.com>
2013-06-28 16:51:25 +0100

    Buttons overlayed on map (no functionality yet)

--- Adme_v1/src/at/adme/app/Map.java
+++ Adme_v1/src/at/adme/app/Map.java
@@ -1,13 +1,19 @@
 package at.adme.app;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.widget.ExpandableListView;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
 
@@ -25,45 +31,75 @@ public class Map extends Activity {
 	private static final LatLng TREASURER = new LatLng(53.344478,-6.261317);
 	private static final LatLng ISSSERV = new LatLng(53.34488,-6.255389);
 	private static final LatLng ADM_RECS = new LatLng(53.344393,-6.258905);
-	//private static final LatLng TRINITY = new LatLng(53.343,-6.254);
+	private static final LatLng TRINITY = new LatLng(53.343,-6.254);
 	private GoogleMap mMap = null;
+	private Marker[] services = new Marker[8];
+	private LinkedList<Marker> events = new LinkedList();
+	private LinkedList<Marker> deals = new LinkedList();
 	
 
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(TRINITY, 14));
         this.mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        placeCollegeMarkers();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(TRINITY, 14));
+        placeCollegeServiceMarkers();
         
     }
     
     // Place markers for college services
-    private void placeCollegeMarkers(){
-    	this.mMap.addMarker(new MarkerOptions()
+    /* Add .snippet(String) for sub-text (Address in this case)
+    * Add .icon(BitmapDescriptor) for choosing icon for marker or change colour
+    * Blue icon:
+    * .icon(BitmapDecriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+    */
+    private void placeCollegeServiceMarkers(){
+    	this.services[0] = this.mMap.addMarker(new MarkerOptions()
     		.position(ADM_RECS)
     		.title("Admissions and Student Records"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[1] = this.mMap.addMarker(new MarkerOptions()
     		.position(ISSSERV)
     		.title("ISS Services"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[2] = this.mMap.addMarker(new MarkerOptions()
         	.position(TREASURER)
         	.title("Treasurer's Office (Fees)"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[3] = this.mMap.addMarker(new MarkerOptions()
         	.position(CAMPSUPER)
         	.title("Campus Superintendent (Lockers)"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[4] = this.mMap.addMarker(new MarkerOptions()
         	.position(HEALTH)
         	.title("College Health Centre"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[5] = this.mMap.addMarker(new MarkerOptions()
         	.position(CAS)
         	.title("Careers Advisory Service"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[6] = this.mMap.addMarker(new MarkerOptions()
         	.position(DISABILITIES)
         	.title("Disabilities Services"));
-    	this.mMap.addMarker(new MarkerOptions()
+    	this.services[7] = this.mMap.addMarker(new MarkerOptions()
         	.position(SU)
         	.title("Students Union"));
     }
+    
+	private void clearCollegeServiceMarkers(){
+    	for(Marker m : this.services){
+    		m.setVisible(false);
+    	}
+    }
+    
+	private void clearEventsMarkers(){
+		for(int i=0; i<events.size(); i++){
+			this.events.get(i).setVisible(false);
+		}
+	}
+
+	private void clearDealsMarkers(){
+		for(int i=0; i<deals.size(); i++){
+			this.deals.get(i).setVisible(false);
+		}
+	}
+
+	
+    
 }
